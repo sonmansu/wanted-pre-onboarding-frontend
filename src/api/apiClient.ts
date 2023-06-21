6628fa2d9e045cf1144744abbbe43e7593f35c7c
Sumin Sohn <soyw31@gmail.com>
2023-06-21 20:35:31 +0900

    [refact] 요청 인터셉터로 authorization 헤더 설정, 환경변수 설정

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,11 +1,25 @@
 import axios from "axios";
-
-const BASE_URL = `https://www.pre-onboarding-selection-task.shop/`;
+import { TOKEN } from "common/utils/constants";
 
 export const axiosClient = axios.create({
-    baseURL: BASE_URL,
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
     },
 });
+
+axiosClient.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem(TOKEN);
+
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+);
--- src/api/lib/todo.ts
+++ src/api/lib/todo.ts
@@ -1,39 +1,15 @@
 import { axiosClient } from "../apiClient";
 
 export function getTodos() {
-    return axiosClient.get("/todos", {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-    });
+    return axiosClient.get("/todos");
 }
 
 export function createTodo(todo: string) {
-    return axiosClient.post(
-        "/todos",
-        { todo },
-        {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-            },
-        }
-    );
+    return axiosClient.post("/todos", { todo });
 }
 export function updateTodo(id: number, todo: string, isCompleted: boolean) {
-    return axiosClient.put(
-        `/todos/${id}`,
-        { todo, isCompleted },
-        {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-            },
-        }
-    );
+    return axiosClient.put(`/todos/${id}`, { todo, isCompleted });
 }
 export function deleteTodo(id: number) {
-    return axiosClient.delete(`/todos/${id}`, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-    });
+    return axiosClient.delete(`/todos/${id}`);
 }
